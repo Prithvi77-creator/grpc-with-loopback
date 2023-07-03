367ab292d6b19350a20be07352e2e96956458327
Prithvipal Singh <prithvipal.singh@edprime.co>
2023-07-03 18:25:18 +0530

    code

--- grpc2.0/dist/__tests__/acceptance/test-helper.d.ts
+++
@@ -1,7 +0,0 @@
-import { GrpcApplication } from '../..';
-import { Client } from '@loopback/testlab';
-export declare function setupApplication(): Promise<AppWithClient>;
-export interface AppWithClient {
-    app: GrpcApplication;
-    client: Client;
-}
